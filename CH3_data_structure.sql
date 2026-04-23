01599d431014b53098c6e7ec4517c88ef19a21c3
yuxuan92 <54770335+yuxuan92@users.noreply.github.com>
2026-04-23 21:19:36 +0800

    Update CH3_data_structure.sql

--- CH3_data_structure.sql
+++ CH3_data_structure.sql
@@ -57,4 +57,44 @@ real_6位數精度
 double_15位數精度
 */--------------------------------------
 
+SELECT
+	numeric_column * 10000000 AS Fixed,
+	real_column * 10000000AS Float
+FROM number_data_types
+WHERE numeric_column = 0.7;
+/*--------------------------------------
+fixed|float
+7000000.00000|6999999.88079071
+浮點數小數位計算會有不精確的問題
+*/--------------------------------------
+
+CREATE TABLE date_time_types (
+	timestamp_column timestamp with time zone,
+	interval_column interval
+);
+
+INSERT INTO date_time_types
+VALUES
+	('2018-12-31 01:00 EST', '2 days'), -時區
+	('2018-12-31 01:00 -8', '1 month'), -與UTC的時差
+	('2018-12-31 01:00 Australia/Melbourne', '1 century'), -區域或位置指定時區
+	(now(), '1 week'); -硬體當下時間
+
+SELECT * FROM date_time_types;
+
+COPY date_time_types TO 'C:\temp\NUM.txt'
+WITH(FORMAT CSV, HEADER, DELIMITER '|');
+/*--------------------------------------
+timestamp_column|interval_column
+2018-12-31 14:00:00+08|2 days
+2018-12-31 17:00:00+08|1 mon
+2018-12-30 22:00:00+08|100 years
+2026-04-22 15:26:55.820941+08|7 days
+
+timestamp_8 bytes, 日期+時間 (with time zone), YYYY-MM-DD HH:MM:SS(ISO格式)
+date_4 bytes, 日期
+time_8 bytes, 時間 (with time zone)
+interval_16 bytes. 時間區隔
+*/--------------------------------------
+
 
