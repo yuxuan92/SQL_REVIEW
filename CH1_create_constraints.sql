1a46701bde2a4b329bc3539d6d86db9c6fe882af
yuxuan92 <54770335+yuxuan92@users.noreply.github.com>
2026-05-04 21:02:48 +0800

    Create CH1_create_constraints.sql

---
+++ CH1_create_constraints.sql
@@ -0,0 +1,8 @@
+CREATE TABLE sales_records (
+    sale_id bigserial PRIMARY KEY,          -- 設定為主鍵，且自動跳號
+    store_name varchar(50) NOT NULL,        -- NOT NULL：這欄一定要填，不填會報錯
+    product_name varchar(50) DEFAULT '未分類', -- DEFAULT：沒填的話，自動填入 '未分類'
+    sale_amount numeric(20, 2) DEFAULT 0,    -- DEFAULT：沒填的話，預設金額為 0
+    sale_date date DEFAULT CURRENT_DATE,    -- DEFAULT：沒填的話，自動抓「今天」
+    is_active boolean DEFAULT true          -- 常用於標記資料是否有效
+);
