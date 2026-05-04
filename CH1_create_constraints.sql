e2d11841cf6dd8d46d4cf20b6f849218e58ba506
yuxuan92 <54770335+yuxuan92@users.noreply.github.com>
2026-05-04 21:07:00 +0800

    Update CH1_create_constraints.sql

--- CH1_create_constraints.sql
+++ CH1_create_constraints.sql
@@ -1,3 +1,6 @@
+--假設有10個欄位，但入資料庫只給1-5個欄位
+--剩下的 6 到 10 欄會怎樣取決當初建表時的設定
+
 CREATE TABLE sales_records (
     sale_id bigserial PRIMARY KEY,          -- 設定為主鍵，且自動跳號
     store_name varchar(50) NOT NULL,        -- NOT NULL：這欄一定要填，不填會報錯
