6974c8fc09741a72742adb173ad4bc5825995b9e
yuxuan92 <54770335+yuxuan92@users.noreply.github.com>
2026-05-18 21:41:51 +0800

    Update CH4_exercise.sql

--- CH4_exercise.sql
+++ CH4_exercise.sql
@@ -1,1 +1,14 @@
+-- p.71練習題
+-- 1.撰寫一道COPY陳述，再加上WITH，以便匯入一個假想的文字檔，他的前幾列會像這樣：
+/*--------
+id:movie:actor
+50:#Mission: Impossible#:Tom Cruise
+--------*/
 
+-- SOL
+-- 1.分隔符號是 ':'
+-- 2.中間有一個 ':'，題目用 '#' 取代 '"'
+
+COPY movies
+FROM ''
+WITH(FORMAT CSV, HEADER, DELIMITER ':', QUOTE '#');
